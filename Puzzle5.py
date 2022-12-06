25c0475b03753b3922c46bdbe72d0652a1dcc9c9
David-John Miller <anoyomouse@gmail.com>
2022-12-06 15:24:11 +0200

    Fix puzzle 5

--- Puzzle5.py
+++ Puzzle5.py
@@ -12,13 +12,15 @@ for x in data:
         break
     for y in range(9):
         val = x[1 + (y * 4)]
-        if val != " ": # and val not in nums:
+        if val != " " and val not in nums:
             stacks[y].append(val)
 
 start_stacks = [x[::-1] for x in stacks]
 
+del stacks
 # %%
-stacks = list(start_stacks)
+stacks_a = [x for x in start_stacks]
+stacks_b = [x for x in start_stacks]
 start = False
 for x in data:
     x = x.strip('\n')
@@ -34,20 +36,32 @@ for x in data:
     stack_from = int(words[3]) - 1
     stack_to = int(words[5]) - 1
 
-    from_data = stacks[stack_from][-move_num:]
-    # Part A is reverse order
-    #from_data.reverse()
+    from_data = stacks_a[stack_from][-move_num:]
+    from_data.reverse()
+    stacks_a[stack_from] = stacks_a[stack_from][:-move_num]
+    stacks_a[stack_to] = stacks_a[stack_to] + from_data
 
-    stacks[stack_from] = stacks[stack_from][:-move_num]
-    stacks[stack_to] += from_data
+    from_data = stacks_b[stack_from][-move_num:]
+    stacks_b[stack_from] = stacks_b[stack_from][:-move_num]
+    stacks_b[stack_to] = stacks_b[stack_to] + from_data
 
 # %%
-for x in stacks:
+print(" === A ===")
+for x in stacks_a:
     print(' '.join(x))
 
-top_box = [x[-1] for x in stacks]
+top_box_a = [x[-1] for x in stacks_a]
 
-print(top_box)
-print(''.join(top_box))
+print(top_box_a)
+print(''.join(top_box_a) + " - TDCHVHJTG")
+
+print(" === B ===")
+for x in stacks_b:
+    print(' '.join(x))
+
+top_box_b = [x[-1] for x in stacks_b]
+
+print(top_box_b)
+print(''.join(top_box_b) + " - NGCMPJLHV")
 
 # %%
