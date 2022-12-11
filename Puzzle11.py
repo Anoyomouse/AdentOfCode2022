1c0942d44d78c73464050c234c028c0affcd3f47
David-John Miller <david-john.miller@nintex.com>
2022-12-11 22:01:08 +0200

    Part A

--- Puzzle11.py
+++ Puzzle11.py
@@ -1,11 +1,10 @@
 # %%
-from typing import List, Dict
+from typing import List, Dict 
 
 class Monkey:
     def __init__(self, _6_lines : List[str]):
-        self.name = int
-        self.items = List[int]
-        self.test_results = Dict[bool, int]
+        self.number = int
+        self.items = []
         self.activity = 0
 
         self.test_results = {}
@@ -17,40 +16,61 @@ class Monkey:
                 self.items = eval("[" + x.split(':')[1].strip(' ') + "]")
             if x.startswith('  Operation:'):
                 self.operation = x.split(':')[1].strip(' ')
+                self.operation_func = self.operation.split(' = ')[1].strip()
             if x.startswith('  Test:'):
                 self.test = x.split(':')[1].strip(' ')
-                divisor = self.test.split(' ')[-1]
-                self.test_func = lambda x: x % divisor
+                self.divisor = int(self.test.split(' ')[-1])
+                self.test_func = lambda x: x % self.divisor
             if x.startswith('    If true'):
-                self.test_results[True] = int(x.split(':')[1].strip(' ').split(' ')[-1])
+                self.test_results[True] = x.split(':')[1].strip(' ').split(' ')[-1]
             if x.startswith('    If false'):
-                self.test_results[False] = int(x.split(':')[1].strip(' ').split(' ')[-1])
+                self.test_results[False] = x.split(':')[1].strip(' ').split(' ')[-1]
 
     def recieve_item(self, item):
         self.items.append(item)
 
-    def do_turn(self, monkeys: List["Monkey"]):
+    def do_turn(self, monkeys: Dict[str, "Monkey"]):
         new = 0
         for old in self.items:
             self.activity += 1
-            exec(self.operation)
-            print(f'Worry level is now: {new}')
+            #- print(f"Worry level: {old}")
+            #- print(f"Worry level operation: {self.operation}")
+            # exec(self.operation, {}, {"old": old, "new": new})
+            new = eval(self.operation_func.replace('old', str(old)))
+            #- print(f'Worry level is now: {new}')
             new = new // 3
-            monkeys[self.test_results[self.test_func(new)]].recieve_item(old)
+            #- print(f'Worry has calmed down to: {new}')
+            # pass_to = self.test_results[self.test_func(new)]
+            pass_to = self.test_results[new % self.divisor == 0]
+            monkeys[pass_to].recieve_item(new)
+        self.items = []
+
+# %%
 
 # %%
 data = [x.strip('\n') for x in open('Puzzle11_input.txt', 'r').readlines()]
 
-monkeys = []
+monkeys = {}
 start = 0
 while True:
-    monkeys.append(Monkey(data[start:start + 6]))
-    start += 1
+    monkey = Monkey(data[start:start + 6])
+    monkeys[monkey.number] = monkey
+    start += 7
     if start >= len(data):
         break
 
-for round in range(1):
-    for x in monkeys:
+for round in range(20):
+    print(f"Starting round {round}")
+    for n in monkeys:
+        x = monkeys[n]
+        print(f"    -- Monkey {x.number} -- Items: {x.items} -- Item: {len(x.items)}")
         x.do_turn(monkeys)
 
+
+top_monkeys = sorted([monkeys[x].activity for x in monkeys], reverse=True)
+
+shenanigans = top_monkeys[0] * top_monkeys[1]
+
+# 276150 is too high
+print(f"Monkey Buisness: {shenanigans} - (182293)")
 # %%
